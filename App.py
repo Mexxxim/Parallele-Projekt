042cf98c893e16d69c958a9ad712f17410fbf6ad
Maxim Matiks <maximmatiks@web.de>
2024-01-17 16:09:19 +0100

    removed migration

--- App.py
+++ App.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+app.app_context().push()
 
 
 class Bookshelf(db.Model):
